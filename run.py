4cdc89549c7522ef03f3909f7d4960eaec564db7
Huddy2022 <mlvnhud@aol.com>
2022-12-16 19:35:42 +0000

    add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -19,17 +18,17 @@ def get_sales_data():
     Get sales figures input from the user.
     """
     while True:
-            print("Please enter sales data from the last market.")
-            print("Data should be six numbers, separated by commas.")
-            print("Example: 10,20,30,40,50,60\n")
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
             
-            data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
             
-            sales_data = data_str.split(",")
+        sales_data = data_str.split(",")
             
-            if validate_data(sales_data):
-                print("Data is valid!")
-                break
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
 
     return sales_data
 
@@ -45,32 +44,12 @@ def validate_data(values):
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
-            )
+                )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
 
     return True
-
-
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
-
-
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list data provided
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
 
 
 def update_worksheet(data, worksheet):
@@ -94,7 +73,7 @@ def caluculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    
+       
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
